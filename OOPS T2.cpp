3cf35a216407dc83f8b50bd0525c5aee2ca84c5a
Pawanesh_191921 <55928870+Pawaneshmishra@users.noreply.github.com>
2021-03-03 15:56:09 +0530

    OOPS TUTORIAL

---
+++ OOPS T2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+class Number                                          //Creating class "Number"
+{
+    public:
+        int power(double x, int y)
+        {
+            int i=pow(x,y);
+            return i;
+        }
+};
+
+int main()
+{
+    int n=2;                                          //Declaring Variables
+    double m;
+
+    cout<<"Enter a Number to get it's power:"<<endl;  //Getting input
+    cin>>m;
+
+    Number a;                  //Creating an Object
+    cout<<a.power(m,n);        //Carrying out power function
+
+    return 0;
+}
