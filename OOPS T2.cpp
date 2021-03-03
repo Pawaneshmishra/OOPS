8d3c38ca71245c45a0bc31530ceee0f10ec9a167
Pawanesh_191921 <55928870+Pawaneshmishra@users.noreply.github.com>
2021-03-03 16:14:09 +0530

    OOPS T2

--- OOPS T2.cpp
+++ OOPS T2.cpp
@@ -17,10 +17,11 @@ int main()
     int n=2;                                          //Declaring Variables
     double m;
 
-    cout<<"Enter a Number to get it's power:"<<endl;  //Getting input
+    cout<<"Enter a Number to get it's power"<<endl;  //Getting input
     cin>>m;
 
     Number a;                  //Creating an Object
+    cout<<"The power of Number is:"<<endl;
     cout<<a.power(m,n);        //Carrying out power function
 
     return 0;
