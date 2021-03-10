eaf08a0e2b36c6077bc3fcc0d61243c16158cbe1
Pawanesh_191921 <55928870+Pawaneshmishra@users.noreply.github.com>
2021-03-10 12:08:32 +0530

    OOPS Tutorial

---
+++ OOPS T4.cpp
@@ -0,0 +1,36 @@
+/*WAP to show the effect of default arguments can be alternatively achieved by overloading.*/
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+float power(float x)                                //Power with 1 parameter
+{
+    return pow(x,2);
+}
+
+float power(float x,float y)                        //Power with 2 parameter
+{
+    return pow(x,y);
+}
+
+void display(float data)                           //Display Function
+{
+    cout<<data<<endl;
+}
+
+int main()                                         //Main Function
+{
+    float a,b,i,j;
+    cout<<"Enter Two number:"<<endl;
+    cin>>a>>b;
+
+    i=power(a);
+    j=power(a,b);
+
+    cout<<"A ^ 2 is : ";
+    display(i);
+    cout<<"A ^ B is : ";
+    display(j);
+    return 0;
+}
